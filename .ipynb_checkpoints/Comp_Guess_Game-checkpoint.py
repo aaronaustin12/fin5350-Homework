74f2f29d9373f1d4779d3370622401f1d4534772
Aaron Austin <aaron.austin12@gmail.com>
2018-10-15 21:21:33 -0600

    homework to keep doing

---
+++ .ipynb_checkpoints/Comp_Guess_Game-checkpoint.py
@@ -0,0 +1,94 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 25,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "The computer guesses... 50\n",
+      "The computer guesses... 25\n",
+      "The computer guesses... 13\n",
+      "The computer guesses... 7\n",
+      "The computer guesses... 4\n",
+      "The computer guesses... 2\n",
+      "The computer guesses... 1\n",
+      "And the computer guessed 1 and thats the right answer!\n"
+     ]
+    }
+   ],
+   "source": [
+    "def computer_guess(num):\n",
+    "    low = 1\n",
+    "    high = 100\n",
+    "    guess = 50\n",
+    "    while guess != num:\n",
+    "        guess = (low+high)//2\n",
+    "        print(\"The computer guesses...\", guess)\n",
+    "        if guess > num:\n",
+    "            high = guess\n",
+    "        elif guess < num:\n",
+    "            low = guess +1\n",
+    "            \n",
+    "    print(\"And the computer guessed\", guess, \"and thats the right answer!\")\n",
+    "\n",
+    "    \n",
+    "def main():\n",
+    "    num = int(input(\"Enter a number:\"))\n",
+    "    if num < 1 or num > 100:\n",
+    "        print(\"Must be in range[1,100]\")\n",
+    "    else:\n",
+    "        computer_guess(num)\n",
+    "        \n",
+    "if __name__ == '__main__':\n",
+    "    main()\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
